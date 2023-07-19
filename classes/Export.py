78a363f9d3248400d7881150b8282d897c5ab441
Rory <rorybennett123@gmail.com>
2023-07-19 15:25:53 +0100

    add export transverse data

--- classes/Export.py
+++ classes/Export.py
@@ -15,7 +15,8 @@ class Export:
         self.totalPatients = eu.getTotalPatients(self.scansPath)
         self.patients = [f'{x:03d}' for x in range(1, self.totalPatients + 1)]
 
-    def exportAllSaveData(self, mainWindow: QWidget):
+    def exportAllSaveData(self):
+        """Export all save data from all patients - For backup."""
         print(f'Exporting all Save Data from all patients...')
         scanTypes = ['Transverse', 'Sagittal']
         savePath = eu.createSaveDataExportDir()
@@ -39,11 +40,12 @@ class Export:
         print(f'\tSExporting completed.')
 
     def exportIPVSagittalData(self, mainWindow: QWidget):
+        """Export sagittal frames for ipv inference."""
         print(f'Exporting Sagittal frames for IPV inference...')
         # Get Save prefix.
         prefix, ok = QInputDialog.getText(mainWindow, 'Select Save for Sagittal Export', 'Enter Prefix:')
         if not ok:
-            print('\tCreate Sagittal Data Cancelled.')
+            print('\tCreate Sagittal IPV Data Cancelled.')
             return
         # Create directories for sagittal training data.
         savePath = eu.createIPVTrainingDirs(Scan.TYPE_SAGITTAL)
@@ -89,3 +91,54 @@ class Export:
                         f'{saveName} ({pointDataDisplay[0][1]}, {pointDataDisplay[0][2]}) '
                         f'({pointDataDisplay[1][1]}, {pointDataDisplay[1][2]})\n')
         print(f'\tExporting completed.')
+
+    def exportIPVTransverseData(self, mainWindow: QWidget):
+        """Export transverse frames for ipv inference."""
+        # Get Save prefix.
+        prefix, ok = QInputDialog.getText(mainWindow, 'Select Save for Transverse Export', 'Enter Prefix:')
+        if not ok:
+            print('\tCreate Transverse IPV Data Cancelled.')
+            return
+        # Create directories for sagittal training data.
+        savePath = eu.createIPVTrainingDirs(Scan.TYPE_TRANSVERSE)
+        # Create training data from each patient.
+        for patient in self.patients:
+            transversePath = f'{self.scansPath}/{patient}/transverse'
+            framePath = os.listdir(Path(transversePath).absolute())[0]
+            scanPath = f'{transversePath}/{framePath}'
+            # Get save directory with given prefix.
+            saveDir = eu.getSaveDirName(scanPath, prefix)
+            if not saveDir:
+                print(f'\tPatient {patient} does not have a matching Save directory with prefix {prefix}.')
+            # Path to PointData.txt file.
+            pointDataPath = f'{scanPath}/Save Data/{saveDir}/PointData.txt'
+            # Get point data in mm from f()ile.
+            pointDataMm = eu.getPointData(pointDataPath)
+            # Get depths of scans.
+            depths = eu.getDepths(scanPath)
+            # Get required IMU data.
+            imuOffset, imuPosition = eu.getIMUData(f'{scanPath}/Save Data/{saveDir}')
+            # Get frames with points on them.
+            framesWithPoints = eu.getFramesWithPoints(scanPath, pointDataMm)
+            if not framesWithPoints:
+                print(f'\tNo frames with point data available.')
+                return
+            # Loop through frames with points on them.
+            for index, frame in enumerate(framesWithPoints, start=1):
+                saveName = f'{patient}_{index}.png'
+                # Save frame to disk.
+                cv2.imwrite(f'{savePath}/Transverse/{saveName}', frame)
+                # Convert points from mm to display/pixel coordinates.
+                pointDataDisplay = []
+                for point in pointDataMm:
+                    pointDisplay = eu.mmToDisplayCoordinates([point[1], point[2]], depths, imuOffset, imuPosition,
+                                                             [frame.shape[1], frame.shape[0]])
+                    pointDataDisplay.append([point[0], pointDisplay[1], pointDisplay[1]])
+                # Ensure there are only 2 points per frame.
+
+                # Save point data.
+                with open(f'{savePath}/Transverse_mark_list.txt', 'a') as pointFile:
+                    pointFile.write(
+                        f'{saveName} ({pointDataDisplay[0][1]}, {pointDataDisplay[0][2]}) '
+                        f'({pointDataDisplay[1][1]}, {pointDataDisplay[1][2]})\n')
+            print(f'\tExporting completed.')
