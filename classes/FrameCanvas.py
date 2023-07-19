31d47bd0d89295fed406d56b4e7272e66e267513
Rory <rorybennett123@gmail.com>
2023-07-19 13:46:11 +0100

    fix background patch colour

--- classes/FrameCanvas.py
+++ classes/FrameCanvas.py
@@ -10,6 +10,6 @@ class FrameCanvas(FigureCanvasQTAgg):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         fig = Figure(dpi=dpi)
         fig.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
-        fig.patch.set_facecolor((1, 1, 1, 1))
         self.axes = fig.add_subplot(111)
+        self.axes.patch.set_facecolor((0.5, 0.5, 0.5, 1))
         super(FrameCanvas, self).__init__(fig)
