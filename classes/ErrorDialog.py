2593e3b30ac51efe6206239c09386aa2f2be9a50
Rory <rorybennett123@gmail.com>
2024-07-19 14:59:35 +0100

    update Error Dialog to show correct file where error occured

--- classes/ErrorDialog.py
+++ classes/ErrorDialog.py
@@ -6,7 +6,7 @@ class ErrorDialog(QDialog):
     Custom dialog to show an error message with the error below the message.
     """
 
-    def __init__(self, parent=None, message=None, e=None):
+    def __init__(self, parent=None, message=None, e=None, f_name=None):
         super().__init__(parent)
 
         self.setWindowTitle("Error")
@@ -15,7 +15,7 @@ class ErrorDialog(QDialog):
 
         layout = QVBoxLayout(self)
         layout.addWidget(QLabel(f'Message: {message}'))
-        layout.addWidget(QLabel(f'Error: {type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}.'))
+        layout.addWidget(QLabel(f'Error: {type(e).__name__} at line {e.__traceback__.tb_lineno} of {f_name}.'))
         layout.addWidget(buttonBox)
 
         buttonBox.accepted.connect(self.accept)
